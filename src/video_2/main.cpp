82eee0f597b1f147c3b399050ad4f143637289c8
zl90 <101400977+zl90@users.noreply.github.com>
2023-09-01 22:31:04 +1000

    Add multidimensional arrays + random numbers

--- src/video_2/main.cpp
+++ src/video_2/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
+    srand(time(NULL)); // Seed the random number generator.
+
     while (true)
     {
         int arr[] = {1, 2, 3, 4, 5}; // Initialise arrays
@@ -15,6 +18,20 @@ int main()
         cout << endl;
         cout << *arr << endl;       // Dereferencing the array points to the first element in the array
         cout << *(arr + 1) << endl; // Arrays are contiguous in memory, so you can access them via pointer arithmetic
+
+        int board[8][8]; // Multidimensional arrays
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                board[i][j] = 0;
+            }
+        }
+
+        // Random numbers:
+        cout << "A random number: " << rand() << endl;
+        cout << "A smaller random number: " << rand() % 10 << endl;
+
         int option;
         cout << "Enter your preferred means of transport (1..3): ";
         cin >> option;
