8cc6466155403ae24aa70633177e808746e6c61f
zl90 <101400977+zl90@users.noreply.github.com>
2023-09-03 14:57:39 +1000

    Add exercise 6

--- src/video_2/exercises.cpp
+++ src/video_2/exercises.cpp
@@ -16,11 +16,22 @@ int factorial(int n)
     }
 }
 
+// Exercise 6: Create a program that calculates the absolute value of a number using the ternary operator.
+signed int abs(signed int n)
+{
+    int result;
+    (n < 0) ? result = n *-1 : result = n;
+    return result;
+}
+
 void runTests()
 {
     factorial(0) == 1 ? cout << "factorial(0):\tpassed!" << endl : cout << "factorial(0):\tfailed..." << endl;
     factorial(4) == 24 ? cout << "factorial(4):\tpassed!" << endl : cout << "factorial(4):\tfailed..." << endl;
     factorial(-1) == 1 ? cout << "factorial(-1):\tpassed!" << endl : cout << "factorial(-1):\tfailed..." << endl;
+
+    abs(-10) == 10 ? cout << "abs(-10):\tpassed!" << endl : cout << "abs(-10):\tfailed... " << abs(-10) << endl;
+    abs(5) == 5 ? cout << "abs(5):\t\tpassed!" << endl : cout << "abs(5):\tfailed..." << endl;
 
     return;
 }
