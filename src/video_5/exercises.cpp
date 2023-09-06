523f0ab2f57ccab0be89926323528aac022507bf
zl90 <101400977+zl90@users.noreply.github.com>
2023-09-06 21:42:47 +1000

    Add exercise 3

--- src/video_5/exercises.cpp
+++ src/video_5/exercises.cpp
@@ -9,17 +9,24 @@ class GameObject
 public:
     int id;
 
+    // Exercise 3: static member data
+    static int instanceCount;
+
     GameObject()
     {
         this->id = 100;
+        this->instanceCount++;
     }
 
     // Exercise 2: non-default constructor
     GameObject(int pId)
     {
         this->id = pId;
+        this->instanceCount++;
     }
 };
+
+int GameObject::instanceCount = 0;
 
 void runTests()
 {
@@ -28,6 +35,7 @@ void runTests()
 
     obj1.id == 100 ? cout << "Test1:\tpassed!\n" : cout << "Test1:\tfailed...\n";
     obj2.id == 45 ? cout << "Test2:\tpassed!\n" : cout << "Test2:\tfailed...\n";
+    GameObject::instanceCount == 2 ? cout << "Test3:\tpassed!\n" : cout << "Test3:\tfailed...\n";
 }
 
 int main()
