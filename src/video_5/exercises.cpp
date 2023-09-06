f046c98e8555b9e9308be8cf5cf243397314e4c9
zl90 <101400977+zl90@users.noreply.github.com>
2023-09-06 21:37:11 +1000

    Add exercise 2

--- src/video_5/exercises.cpp
+++ src/video_5/exercises.cpp
@@ -13,13 +13,21 @@ public:
     {
         this->id = 100;
     }
+
+    // Exercise 2: non-default constructor
+    GameObject(int pId)
+    {
+        this->id = pId;
+    }
 };
 
 void runTests()
 {
     GameObject obj1;
+    GameObject obj2(45);
 
     obj1.id == 100 ? cout << "Test1:\tpassed!\n" : cout << "Test1:\tfailed...\n";
+    obj2.id == 45 ? cout << "Test2:\tpassed!\n" : cout << "Test2:\tfailed...\n";
 }
 
 int main()
