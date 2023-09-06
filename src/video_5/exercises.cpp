e94587741b99ed543ddfb157a8d3a35009e23254
zl90 <101400977+zl90@users.noreply.github.com>
2023-09-06 22:19:57 +1000

    Add exercise 6

--- src/video_5/exercises.cpp
+++ src/video_5/exercises.cpp
@@ -67,6 +67,14 @@ public:
         this->location = location;
         this->instanceCount++;
     }
+
+    GameObject(const GameObject &obj)
+    {
+        this->id = obj.id;
+        // Exercise 6: deep copy:
+        this->location = new Location(obj.location->x, obj.location->y, obj.location->z);
+        this->instanceCount++;
+    }
 };
 
 int GameObject::instanceCount = 0;
@@ -75,12 +83,18 @@ void runTests()
 {
     GameObject obj1(100, new Location(1, 1, 1));
     GameObject obj2(45, new Location(2, 2, 2));
+    GameObject obj6 = obj2;
 
     obj1.id == 100 ? cout << "Test1:\tpassed!\n" : cout << "Test1:\tfailed...\n";
     obj2.id == 45 ? cout << "Test2:\tpassed!\n" : cout << "Test2:\tfailed...\n";
-    GameObject::instanceCount == 2 ? cout << "Test3:\tpassed!\n" : cout << "Test3:\tfailed...\n";
+    GameObject::instanceCount == 3 ? cout << "Test3:\tpassed!\n" : cout << "Test3:\tfailed...\n";
     obj1.location->x == 1 ? cout << "Test4:\tpassed!\n" : cout << "Test4:\tfailed...\n";
-    obj2.location->x == 2 ? cout << "Test4:\tpassed!\n" : cout << "Test4:\tfailed...\n";
+    obj2.location->x == 2 ? cout << "Test5:\tpassed!\n" : cout << "Test5:\tfailed...\n";
+
+    obj6.location->Set(8, 8, 8);
+
+    obj2.location->x == 2 ? cout << "Test6:\tpassed!\n" : cout << "Test6:\tfailed...\n";
+    obj6.location->x == 8 ? cout << "Test7:\tpassed!\n" : cout << "Test7:\tfailed...\n";
 }
 
 int main()
