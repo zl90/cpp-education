d757876f0ded975067f5bff27188e23858eb1d06
mb541 <mb541@protonmail.com>
2023-09-09 21:32:20 +1000

    Complete video 9

---
+++ src/video_9/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <exception>
+#include <cassert>
+
+using namespace std;
+
+class IOException : public exception
+{
+public:
+    int data;
+    IOException(int id) : data(id) {}
+    virtual const char *what() const throw()
+    {
+        return "Controller Error";
+    }
+};
+
+float divide(float a, float b)
+{
+
+    try
+    {
+        if (b == 0.0)
+        {
+            throw("Error. Divided by zero");
+            return -1;
+        }
+        else
+        {
+            return a / b;
+        }
+    }
+    catch (char *error)
+    {
+        cout << error << endl;
+        return -1;
+    }
+    catch (...)
+    {
+        cout << "Unkown error" << endl;
+        return -1;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    assert(1 < 2);
+
+    cout << divide(4, 3) << endl;
+    cout << divide(10, 0) << endl;
+
+    IOException eio(100);
+
+    try
+    {
+        throw(eio);
+    }
+    catch (IOException &e)
+    {
+        cout << e.what() << " Error number: " << e.data << endl;
+    }
+
+    return 0;
+}
