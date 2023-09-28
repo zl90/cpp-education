b7c5109ea2019870dee6ee1ea3a6cfc32441f037
zl90 <101400977+zl90@users.noreply.github.com>
2023-09-28 23:05:27 +1000

    Finish Debugging with GDB video

---
+++ src/debugging-with-gdb/1_error.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <random>
+
+int main()
+{
+    // Create random number generator
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution dist(0, 100);
+
+    // Run a performing some calculations
+    int sum = 0;
+    while (true)
+    {
+        // Get a divisor
+        auto divisor = dist(mt);
+
+        // Add some value to sum
+        // This has a bug! Our random number genreator can return 0!
+        sum += 10 % divisor;
+
+        // Break out with some condition
+        if (sum > 200)
+            break;
+    }
+
+    std::cout << sum << '\n';
+    return 0;
+}
