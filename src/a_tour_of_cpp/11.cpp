4fb6817daf5e676bed5e8f141b8a26447e9ad9ed
zl90 <mb541@protonmail.com>
2024-01-09 22:02:10 +1100

    Test size of empty class

---
+++ src/a_tour_of_cpp/11.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Test
+{
+};
+
+int main()
+{
+    Test a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z,
+        aa, ba, ca, da, ea, fa, ga, ha, ia, ja, ka, la, ma, na, oa, pa, qa, ra, sa, ta, ua, va, wa, xa, ya, za,
+        ab, bb, cb, db, eb, fb, gb, hb, ib, jb, kb, bl, mb, nb, ob, pb, qb, rb, sb, tb, ub, vb, wb, xb, yb, zb,
+        ac, bc, cc, dc, ec, fc, gc, hc, ic, jc, kc, lc, mc, nc, oc, pc, qc, rc, sc, tc, uc, vc, wc, xc, yc, zc,
+        ad, bd, cd, dd, ed, fd, gd, hd, id, jd, kd, ld, md, nd, od, pd, qd, rd, sd, td, du, vd, wd, xd, yd, zd,
+        ae, be, ce, de, ee, fe, ge, he, ie, je, ke, le, me, ne, oe, pe, qe, re, se, te, ue, ve, we, xe, ye, ze,
+        af, bf, cf, df, ef, ff, gf, hf, ygg, jf, kf, lf, mf, nf, of, pf, qf, rf, sf, tf, uf, vf, wf, xf, yf, zf,
+        ag, bg, cg, dg, eg, fg, gg, hg, ig, jg, kg, lg, mg, ng, og, pg, qg, rg, sg, tg, ug, vg, wg, xg, yg, zg,
+        ah, bh, ch, dh, eh, fh, gh, hh, ih, jh, kh, lh, mh, nh, oh, ph, qh, rh, sh, th, uh, vh, wh, xh, yh, zh,
+        ah1, bh1, ch1, dh1, eh1, fh1, gh1, hh1, ih1, jh1, kh1, lh1, mh1, nh1, oh1, p1h, q01h, rh1, sh1, th1, uh1, vh1, wh1, xh1, yh1, zh1,
+        ai, bi, ci, di, ei, fi, gi, hi, ii, ji, ki, li, mi, ni, oi, pi, qi, ri, si, ti, ui, vi, wi, xi, yi, zi;
+
+    std::cout << sizeof(a) << '\n';  // output: 1
+    std::cout << sizeof(zi) << '\n'; // output: 1
+}
