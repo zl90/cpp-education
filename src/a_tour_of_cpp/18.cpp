18109140508a14f17048b9f66613372c35428447
zl90 <mb541@protonmail.com>
2024-01-16 21:22:19 +1100

    Practice using mutex

--- src/a_tour_of_cpp/18.cpp
+++ src/a_tour_of_cpp/18.cpp
@@ -1,20 +1,37 @@
 #include <thread>
 #include <iostream>
+#include <vector>
+#include <mutex>
+
+static int count = 0;
+
+std::mutex m;
 
 void f()
 {
-    std::cout << "Hello ";
-}
-
-void g()
-{
-    std::cout << "Parallel World!\n";
-}
+    std::scoped_lock lock{m}; // Acquire mutex
+    count = count + 1;
+} // Mutex is implicitly released when lock is destructed
 
 int main()
 {
-    std::jthread t1{f};
-    std::jthread t2{g};
+    for (int j = 0; j < 20; j++)
+    {
+        std::vector<std::thread> threads;
+
+        for (int i = 0; i < 1000; i++)
+        {
+            threads.push_back(std::thread(f));
+        }
+
+        for (int i = 0; i < 1000; i++)
+        {
+            threads[i].join();
+        }
+
+        std::cout << "Count: " << count << '\n';
+        count = 0;
+    }
 
     return 0;
 }
