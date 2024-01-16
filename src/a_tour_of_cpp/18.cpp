1c90c4f437cabc2749751d999912620e5cb218fa
zl90 <mb541@protonmail.com>
2024-01-16 22:05:25 +1100

    Practice using chrono

--- src/a_tour_of_cpp/18.cpp
+++ src/a_tour_of_cpp/18.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <mutex>
+#include <chrono>
 
 static int count = 0;
 
@@ -15,6 +16,7 @@ void f()
 
 int main()
 {
+    // Making a thousand threads (note: without a mutex and a thread lock, the results of the following code is non-deterministic):
     for (int j = 0; j < 20; j++)
     {
         std::vector<std::thread> threads;
@@ -32,6 +34,13 @@ int main()
         std::cout << "Count: " << count << '\n';
         count = 0;
     }
+
+    // Waiting inside a thread:
+    auto t0 = std::chrono::high_resolution_clock::now();
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    auto t1 = std::chrono::high_resolution_clock::now();
+
+    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " nanoseconds passed\n";
 
     return 0;
 }
