dcc988c5b7d89f1c53cf7d9d1b9bad232baa077d
mb541 <mb541@protonmail.com>
2024-05-22 23:42:28 +1000

    Complete Bronze problem

---
+++ src/USACO/bronze/speeding_ticket.cpp
@@ -0,0 +1,44 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	freopen("shell.in", "r", stdin);
+
+	int result = 0;
+
+	vector<int> N = {};
+	vector<int> M = {};
+
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		int miles, speed_limit;
+		cin >> miles >> speed_limit;
+
+		for (int j = 0; j < miles; j++) {
+			N.emplace_back(speed_limit);
+		}
+	}
+
+	for (int i = 0; i < m; i++) {
+		int miles, speed;
+		cin >> miles >> speed;
+
+		for (int j = 0; j < miles; j++) {
+			M.emplace_back(speed);
+		}
+	}
+
+	for (size_t i = 0; i < N.size(); i++) {
+		int speed_difference = M[i] - N[i];
+
+		result = max(speed_difference, result);
+	}
+
+	freopen("shell.out", "w", stdout);
+
+	cout << result;
+}
