3cc4a1f7859a9ca07073e7f1d00a24db1e75aa10
mb541 <mb541@protonmail.com>
2024-08-13 21:14:20 +1000

    Solve binary search medium

---
+++ src/grokking/binary-search/ceiling_of_a_number.cpp
@@ -0,0 +1,45 @@
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+class CeilingOfANumber {
+ public:
+  static int searchCeilingOfANumber(const vector<int>& arr, int key) {
+    if (arr.empty()) return -1;
+    int start = 0, end = arr.size() - 1;
+    while (true) {
+      int i = (start + end) / 2;
+      if (arr[i] == key) return i;
+      if (start == end) {
+        // linear search mode
+        while (i < arr.size()) {
+          if (arr[i] >= key) {
+            return i;
+          }
+          i++;
+        }
+        return -1;
+      } else {
+        // binary search mode
+        if (arr[i] > key) {
+          end = i - 1;
+        } else {
+          start = i + 1;
+        }
+      }
+    }
+  }
+};
+
+int main(int argc, char* argv[]) {
+  cout << CeilingOfANumber::searchCeilingOfANumber(vector<int>{4, 6, 6}, 6)
+       << endl;
+  cout << CeilingOfANumber::searchCeilingOfANumber(vector<int>{1, 3, 8, 10, 15},
+                                                   12)
+       << endl;
+  cout << CeilingOfANumber::searchCeilingOfANumber(vector<int>{4, 6, 10}, 17)
+       << endl;
+  cout << CeilingOfANumber::searchCeilingOfANumber(vector<int>{4, 6, 10}, -1)
+       << endl;
+}
