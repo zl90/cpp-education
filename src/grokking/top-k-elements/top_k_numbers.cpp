8fcbf9fadb2db1ea06ff42532b2dd4a49c538ead
mb541 <mb541@protonmail.com>
2024-08-17 16:39:20 +1000

    Solve top K elements easy

---
+++ src/grokking/top-k-elements/top_k_numbers.cpp
@@ -0,0 +1,48 @@
+using namespace std;
+
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+class KLargestNumbers {
+ public:
+  static vector<int> findKLargestNumbers(const vector<int>& nums, int k) {
+    if (nums.empty()) return {};
+    vector<int> results;
+    priority_queue<int> heap;
+    for (const auto& element : nums) {
+      heap.push(element);
+    }
+    for (int i = 0; i < min(k, (int)heap.size()); i++) {
+      results.push_back(heap.top());
+      heap.pop();
+    }
+    return results;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  vector<int> result =
+      KLargestNumbers::findKLargestNumbers(vector<int>{3, 1, 5, 12, 2, 11}, 3);
+  cout << "Here are the top K numbers: ";
+  for (auto num : result) {
+    cout << num << " ";
+  }
+  cout << endl;
+
+  result =
+      KLargestNumbers::findKLargestNumbers(vector<int>{5, 12, 11, -1, 12}, 3);
+  cout << "Here are the top K numbers: ";
+  for (auto num : result) {
+    cout << num << " ";
+  }
+  cout << endl;
+
+  result = KLargestNumbers::findKLargestNumbers(vector<int>{5, 5, 5, 5}, 3);
+  cout << "Here are the top K numbers: ";
+  for (auto num : result) {
+    cout << num << " ";
+  }
+  cout << endl;
+}
