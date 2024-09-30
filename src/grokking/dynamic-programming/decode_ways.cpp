f5336762cc6ebf99edbac92ded963890aa025f7c
zl90 <mb541@protonmail.com>
2024-10-01 09:01:19 +1000

    Attempt DP medium

--- src/grokking/dynamic-programming/decode_ways.cpp
+++ src/grokking/dynamic-programming/decode_ways.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class SolutionRecursive {
 public:
   int numDecodings(string s) { return dfs(0, s); }
 
@@ -20,5 +20,29 @@ private:
     if (larger < 1 || larger > 26)
       return dfs(i + 1, s);
     return dfs(i + 1, s) + dfs(i + 2, s);
+  }
+};
+
+#include <cstdlib>
+class SolutionDP {
+public:
+  int numDecodings(string s) {
+    int a = 0, b = 1;
+    if (s[s.size() - 1] != '0') {
+      a = 1;
+    }
+    for (int i = s.size() - 2; i >= 0; i--) {
+      int curr = s[i] - '0';
+      int next = (curr * 10) + (s[i + 1] - '0');
+      int c = 0;
+      if (curr >= 1 && next >= 1 && next <= 26) {
+        c = a + b;
+      } else if (curr >= 1) {
+        c = b;
+      }
+      b = a;
+      a = c;
+    }
+    return a;
   }
 };
