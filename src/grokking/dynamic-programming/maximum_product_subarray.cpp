fb3afd932973bdc1f798c345fb4cedfbad43a3f8
zl90 <mb541@protonmail.com>
2024-10-10 08:51:13 +1100

    Solve DP Medium

--- src/grokking/dynamic-programming/maximum_product_subarray.cpp
+++ src/grokking/dynamic-programming/maximum_product_subarray.cpp
@@ -16,3 +16,25 @@ public:
     return maxProd;
   }
 };
+
+class SolutionDP {
+public:
+  int maxProduct(vector<int> &nums) {
+    if (nums.empty())
+      return 0;
+    // Kadanes algorithm --> learn this!
+    int n = nums.size(), left = 1, right = 1, result = nums[0];
+    for (int i = 0; i < n; i++) {
+      if (left == 0) {
+        left = 1;
+      }
+      if (right == 0) {
+        right = 1;
+      }
+      left = left * nums[i];
+      right = right * nums[n - 1 - i];
+      result = max({result, left, right});
+    }
+    return result;
+  }
+};
